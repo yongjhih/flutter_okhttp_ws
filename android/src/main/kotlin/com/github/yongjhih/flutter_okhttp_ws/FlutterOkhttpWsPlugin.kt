41a9fc527c3ea6dcdabd194bc8402c927a2f324d
Andrew Chen <yongjhih@gmail.com>
2019-06-24 15:40:14 -0700

    Implement connect and send

--- android/src/main/kotlin/com/github/yongjhih/flutter_okhttp_ws/FlutterOkhttpWsPlugin.kt
+++ android/src/main/kotlin/com/github/yongjhih/flutter_okhttp_ws/FlutterOkhttpWsPlugin.kt
@@ -5,6 +5,8 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import okhttp3.*
+import okio.ByteString
 
 class FlutterOkhttpWsPlugin: MethodCallHandler {
   companion object {
@@ -15,11 +17,52 @@ class FlutterOkhttpWsPlugin: MethodCallHandler {
     }
   }
 
+  lateinit var websocket: WebSocket
+  var onMessage: (String) -> Unit = {}
+  var onClosed: (Int, String) -> Unit = { _, _ -> }
+
   override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    when (call.method) {
+      "connect" -> {
+        val url: String = call.argument("url")!!
+        val client = OkHttpClient.Builder()
+            .apply {
+              hostnameVerifier { _, _ -> true }
+            }
+            .build()
+        websocket = client.newWebSocket(Request.Builder().url(url).build(), object : WebSocketListener() {
+          override fun onOpen(webSocket: WebSocket, response: Response) {
+            result.success(response.body().toString())
+          }
+
+          override fun onMessage(webSocket: WebSocket, text: String) {
+            onMessage(text)
+          }
+
+          override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+            result.error(t.message, response?.body().toString(), response)
+          }
+
+          override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+            onClosed(code, reason)
+          }
+        })
+      }
+      "send" -> {
+        onMessage = {
+          result.success(it)
+        }
+        websocket.send(call.argument<String>("message")!!)
+      }
+      "close" -> {
+        onClosed = { code, _ ->
+          result.success(code)
+        }
+        websocket.close(0, null)
+      }
+      else -> {
+        result.error("Method not found", null, null)
+      }
     }
   }
 }
--- lib/flutter_okhttp_ws.dart
+++ lib/flutter_okhttp_ws.dart
@@ -6,8 +6,13 @@ class FlutterOkhttpWs {
   static const MethodChannel _channel =
       const MethodChannel('flutter_okhttp_ws');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<String> get connect async {
+    return await _channel.invokeMethod('connect');
+  }
+  static Future<String> get send async {
+    return await _channel.invokeMethod('send');
+  }
+  static Future<int> get close async {
+    return await _channel.invokeMethod('close');
   }
 }
